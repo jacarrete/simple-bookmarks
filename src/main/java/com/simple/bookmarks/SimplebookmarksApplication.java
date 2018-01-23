c3708d4b443118564d58e101e42a5435f3ab1dc0
Javier Carretero <jcarretero@sis.tv>
2018-01-23 10:40:44 +0000

    Initial commit - Spring Security plus jsp

---
+++ src/main/java/com/simple/bookmarks/SimplebookmarksApplication.java
@@ -0,0 +1,12 @@
+package com.simple.bookmarks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplebookmarksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplebookmarksApplication.class, args);
+	}
+}
---
+++ src/main/java/com/simple/bookmarks/config/WebConfig.java
@@ -0,0 +1,34 @@
+package com.simple.bookmarks.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+/**
+ * Created by jcarretero on 22/01/2018.
+ */
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/*.js/**").addResourceLocations("/ui/static/");
+        registry.addResourceHandler("/*.css/**").addResourceLocations("/ui/static/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("login");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+//    @Bean
+//    public InternalResourceViewResolver setupViewResolver()  {
+//        InternalResourceViewResolver resolver =  new InternalResourceViewResolver();
+//        resolver.setPrefix ("/ui/jsp/");
+//        resolver.setSuffix (".jsp");
+//        resolver.setViewClass (JstlView.class);
+//        return resolver;
+//    }
+
+}
